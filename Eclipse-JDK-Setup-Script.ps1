db37919cb9f09db1984d9abcad5bab2121d5a1aa
normalian <warito_hutsu@hotmail.com>
2016-05-22 10:45:01 +0900

    first commit

---
+++ Eclipse-JDK-Setup-Script.ps1
@@ -0,0 +1,51 @@
+function DLUnzip {
+  $zipUrl  = $args[0]
+  $zipName = [System.IO.Path]::GetFileName($zipUrl)
+  $basedir = $args[1]
+  Set-Location $basedir
+
+  # ZIP download
+  $zipPath = [System.IO.Path]::Combine($basedir, $zipName)
+  Write-Output "Downloading start... $args"
+  Invoke-WebRequest -Uri $zipUrl -OutFIle $zipPath
+
+  # ZIP expansion
+  $sh = New-Object -ComObject Shell.Application
+  $unzipDirObj = $sh.NameSpace($basedir)
+  $zipPathObj = $sh.NameSpace($zipPath)
+  Write-Output "expansion zip file... $zipPath"
+  $unzipDirObj.CopyHere($zipPathObj.Items())
+}
+mkdir c:\opt
+mkdir c:\opt\workspace
+
+#download and uznip Eclipse and JDK
+$jdkeclipseHome  = "c:\opt\"
+$eclipseUrl      = $Args[0] # "http://mirrors.neusoft.edu.cn/eclipse/technology/epp/downloads/release/mars/2/eclipse-jee-mars-2-win32-x86_64.zip"
+$jdkUrl          = $Args[1] # "http://azure.azulsystems.com/zulu/zulu8.13.0.5-jdk8.0.72-win_x64.zip"
+
+DLUnzip $jdkUrl $jdkeclipseHome
+DLUnzip $eclipseUrl $jdkeclipseHome
+
+# update PATH environment variable
+$user_path = [Environment]::GetEnvironmentVariable('PATH', 'Machine')
+$user_path += ";$jdkeclipseHome" + [System.IO.Path]::GetFileNameWithoutExtension($jdkUrl) + "\bin"
+[Environment]::SetEnvironmentVariable('PATH', $user_path, 'Machine')
+
+# update JAVA_HOME environment variable
+$java_home = [Environment]::GetEnvironmentVariable('JAVE_HOME', 'Machine')
+$java_home = $jdkeclipseHome + [System.IO.Path]::GetFileNameWithoutExtension($jdkUrl)
+[Environment]::SetEnvironmentVariable('JAVA_HOME', $java_home, 'Machine')
+
+#Create Eclipse shortcut to Desktop
+$objShell = New-Object -ComObject WScript.Shell
+$strDesktop = [Environment]::GetFolderPath('Desktop')
+$strFileName = $strDesktop + "\Eclipse.lnk"
+$strTargetPath = "$jdkeclipseHome\eclipse\eclipse.exe"
+$arguments = "-data $jdkeclipseHome\workspace"
+$objShortCut = $objShell.CreateShortcut($strFileName)
+$objShortCut.TargetPath = $strTargetPath
+$objShortCut.Arguments = $arguments 
+$objShortCut.Save()
+
+Write-Output "end script"
